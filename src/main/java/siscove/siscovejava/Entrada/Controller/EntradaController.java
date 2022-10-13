5a0cbecf60b9a7f9acb048c10c80941c8a140ea7
Luis Gustavo Sousa Carneiro <lluis.gusttavosc@gmail.com>
2022-10-13 16:44:56 -0300

    Pequenas modificações nos pacotes Entrada Fornecedor e Deposito

--- src/main/java/siscove/siscovejava/Entrada/Controller/EntradaController.java
+++ src/main/java/siscove/siscovejava/Entrada/Controller/EntradaController.java
@@ -1,8 +1,10 @@
 package siscove.siscovejava.Entrada.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,17 +23,29 @@ public class EntradaController extends BaseController {
 	private EntradaService entradaService;
 
 	@RequestMapping(value = "/listar", method = RequestMethod.GET, consumes = { "*/*" })
-	public EnvelopeResponse<List<EntradaDto>> ListarEntrada() {
+	public EnvelopeResponse<List<EntradaDto>> getListarEntrada() {
 		return entradaService.getListarEntrada();
 	}
-	@RequestMapping(value="/salvar", method = RequestMethod.POST, consumes = {"*/*"})
-	public EnvelopeResponse<EntradaDto> salvarEntrada(@RequestBody EntradaDto entradaDto){
+
+	@RequestMapping(value = "/salvar", method = RequestMethod.POST, consumes = { "*/*" })
+	public EnvelopeResponse<EntradaDto> salvarEntrada(@RequestBody EntradaDto entradaDto) {
 		EnvelopeResponse<EntradaDto> retorno = entradaService.salvar(entradaDto);
 		return retorno;
 	}
-	
+
 	@RequestMapping(value = "/listar/ativos", method = RequestMethod.GET, consumes = { "*/*" })
 	public EnvelopeResponse<List<EntradaDto>> getListarEntradaAtivos() {
 		return entradaService.getListarEntradaAtivos();
 	}
+
+	@RequestMapping(value = "/produto", method = RequestMethod.POST, consumes = { "*/*" })
+	public List<String> reniciar(@PathVariable String termo) {
+		List<String> retorno = new ArrayList<String>();
+		return retorno;
+	}
+	@RequestMapping(value = "/listar/byTermo", method = RequestMethod.POST, consumes = { "*/*" })
+	public EnvelopeResponse<List<EntradaDto>> getListarEntradas(@RequestBody String txtTermo) {
+		return entradaService.getListarEntradas(txtTermo);
+	}
+	
 }
