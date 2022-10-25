6808632bad339979c33fc1d33009d056a79dadb8
Murilo <murilolopesfreire21@gmail.com>
2022-10-25 16:48:41 -0300

    Ajustes para o funcionamento da tela de venda

--- src/main/java/siscove/siscovejava/Cliente/Controller/ClienteController.java
+++ src/main/java/siscove/siscovejava/Cliente/Controller/ClienteController.java
@@ -3,6 +3,7 @@ package siscove.siscovejava.Cliente.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,12 +24,22 @@ public class ClienteController extends BaseController {
 	
 	@RequestMapping(value = "/listar/byTermo", method = RequestMethod.POST, consumes = { "*/*" })
 	public EnvelopeResponse<List<ClienteDto>> getListarClientes(@RequestBody String txtTermo) {
-		return clienteService.getListarClientes(txtTermo);
+		return clienteService.getListaClientes(txtTermo);
 	}
 	
 	@RequestMapping(value="/salvar", method = RequestMethod.POST, consumes = {"*/*"})
 	public EnvelopeResponse<ClienteDto> salvarCliente(@RequestBody ClienteDto clienteDto){
 		EnvelopeResponse<ClienteDto> retorno = clienteService.salvar(clienteDto);
 		return retorno;
+	}
+	
+	@RequestMapping(value = "/listar/byTermo/{txtTermo}", method = RequestMethod.GET, consumes = { "*/*" })
+	public List<ClienteDto> getListarClientesAutoComplete(@PathVariable String txtTermo) {
+		return clienteService.getListaClientes(txtTermo).getObjeto();
+	}
+	
+	@RequestMapping(value = "/listar/byCpfCnpj/{nroDoc}", method = RequestMethod.GET, consumes = { "*/*" })
+	public List<ClienteDto> getListaClientesByDocumentoAutoComplete(@PathVariable String nroDoc) {
+		return clienteService.getListaClienteByDocumento(nroDoc).getObjeto();
 	}
 }
